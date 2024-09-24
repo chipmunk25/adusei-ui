aab1538670ad7fb9ed7c19ac8be59d7d29d235b7
Desmond Kofi Adusei <desdhi24@gmail.com>
2024-09-24 09:00:58 +0000

    chore: worked on the number input and the textarea ring and outline

--- lib/components/ui/input.tsx
+++ lib/components/ui/input.tsx
@@ -45,7 +45,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            "flex h-10 w-full rounded-md border border-input bg-white px-3 py-2 text-sm ring-offset-zinc-900 transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-300 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-zinc-900 disabled:cursor-not-allowed disabled:opacity-50",
+            "flex h-10 w-full rounded-md border border-neutral-400 bg-white px-3 py-2 text-sm ring-offset-zinc-900 transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-300 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-zinc-900 disabled:cursor-not-allowed disabled:opacity-50",
             prefix && "pl-8",
             suffix && "pr-8",
             // !password && "uppercase",
